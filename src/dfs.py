d22e42c53ec5756d962c6596e2fbe33684b6af35
Ivan Ristović <ivo.ristovic@gmail.com>
2017-03-25 16:34:01 +0100

    DFS improvement

--- src/dfs.py
+++ src/dfs.py
@@ -10,15 +10,53 @@ class DFS(traverser.Traverser):
 		self.path = deque()
 		self.path.append(self.maze.start)
 
-		#self.dfs_traverse_recursive(self.maze.start)
 		self.dfs_traverse_iterative()
 
-		if self.solved:
-			return self.path, self.steps
-		else:
-			return [], 0
+		return list(self.path), self.steps
 
 
+	def dfs_traverse_iterative(self):
+		# Setting start node
+		node = self.maze.start
+		self.visited[node.x * self.maze.w + node.y] = True
+
+		while (self.path):
+			# Take a look at top of the stack (DEQUE Y U NO IMPLEMENT POP?)
+			node = self.path.pop()
+			self.path.append(node)
+			# If that is the ending node, then we are done
+			if node == self.maze.end:
+				self.solved = True
+				return
+			# Now we need to check if that node has visited neighbors
+			has_nonvisited_neighbors = False
+			for n in node.neighbors:
+				# If we find one who isn't visited
+				if self.visited[n.x * self.maze.w + n.y] == False:
+					# Put it on top of the stack and mark it as visited
+					has_nonvisited_neighbors = True
+					self.visited[n.x * self.maze.w + n.y] = True
+					self.path.append(n)
+					self.steps += 1
+					break
+			# If we didn't find any neighbors that aren't visited, then pop
+			if has_nonvisited_neighbors == False:
+				self.path.pop()
+
+			# TODO HACK maybe add label on top of the while loop and just
+			# continue that loop when we find one non-visited node?
+			# Then there will be no need for this indicator, something like:
+			# ...
+			# for all neighbors:
+			# 	if self.visited[..] = false
+			# 		push it
+			# 		mark it
+			#		continue while
+			# path.pop()	- no need for indicator since if we made it here,
+			#                 we are sure that it is a dead end
+
+
+	# Recursive variant, deprecated
 	def dfs_traverse_recursive(self, node):
 		if node == self.maze.end:
 			self.solved = True
@@ -34,22 +72,3 @@ class DFS(traverser.Traverser):
 						self.path.pop()
 					else:
 						return
-
-
-	def dfs_traverse_iterative(self):
-		node = self.maze.start
-		stack = deque()
-		stack.append((node, [node]))
-		self.visited[node.x * self.maze.w + node.y] = True
-
-		while (stack):
-			(node, local_path) = stack.pop()
-			if node == self.maze.end:
-				self.solved = True
-				self.path = local_path
-				return
-			for n in node.neighbors:
-				if self.visited[n.x * self.maze.w + n.y] == False:
-					self.visited[n.x * self.maze.w + n.y] = True
-					stack.append((n, local_path + [n]))
-					self.steps += 1
